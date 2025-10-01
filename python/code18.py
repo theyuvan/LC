6eed4fcae3bc34cf0162991bfa0909be80cedc3f
theyuvan <yuvan.r2005@gmail.com>
2025-10-01 11:27:31 +0530

    Binary Tree Preorder Traversal

---
+++ python/code18.py
@@ -0,0 +1,47 @@
+# Question : 
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
+# If the input is [1,2,3,4,5,null,8,null,null,6,7,9], then the output should be [1,2,4,5,3,6,7,8,9].
+
+
+# Answer
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+def binary_tree(arr):
+    if not arr:
+        return None
+    
+    root = TreeNode(arr[0])
+    queue = [root]
+    i = 1
+    while i < len(arr):
+        node = queue.pop(0)
+        if i < len(arr) and arr is not None:
+            node.left = TreeNode(arr[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(arr) and arr is not None:
+            node.right = TreeNode(arr[i])
+            queue.append(node.right)
+        i += 1
+    
+    return root
+
+sol = Solution()
+arr = [1,None,2,3]
+root = binary_tree(arr)
+res = sol.preorderTraversal(root)
+print(res)
