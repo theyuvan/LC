76ce921b858f21aa891b1bbbab64db38d02c5ba3
theyuvan <yuvan.r2005@gmail.com>
2025-10-05 22:34:50 +0530

    Power of Four

---
+++ python/code30.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n % 4 != 0:
+            return False
+        return self.isPowerOfFour(n // 4)
+
+sol = Solution()
+n = 16
+res = sol.isPowerOfFour(n)
+print(res)
